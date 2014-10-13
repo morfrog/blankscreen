dcb1d927a0addf70eb0c2eaa6219d94abbc8581d
drew <drewfrog@gmail.com>
2014-10-13 21:48:08 +1000

    Initial version. See blankscreen.codeplex.com for more information.

---
+++ BlankScreen/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlankScreen
+{
+    public partial class Form1 : Form
+    {
+        FormCursorAutoHide _formCursor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs ev)
+        {
+            _formCursor = new FormCursorAutoHide(this, 3000);
+            if (Screen.AllScreens.Count() > 1)
+            {
+                int index = 0;
+                foreach( var screen in Screen.AllScreens )
+                {
+                    var name = string.Format("Screen &{0}", index+1);
+                    int screenIndex = index;
+                    var handler = new EventHandler((o, e) => screenHandler_Click(o, ev, screenIndex));
+                    contextMenuStrip1.Items.Add(name, null, handler);
+                    index++;
+                }
+            }
+            
+            contextMenuStrip1.Items.Add(new ToolStripSeparator());
+            contextMenuStrip1.Items.Add(new ToolStripMenuItem("&Topmost", null, new EventHandler(topmostToolStripMenuItem_Click)) { CheckOnClick = true, Checked = Settings1.Default.Topmost });
+            contextMenuStrip1.Items.Add(new ToolStripMenuItem("&Minimize", null, new EventHandler(minimiseToolStripMenuItem_Click)));
+            contextMenuStrip1.Items.Add("E&xit", null, new EventHandler(exitToolStripMenuItem_Click));
+
+            this.TopMost = Settings1.Default.Topmost;
+            if( Settings1.Default.ScreenIndex >= 0 )
+            {
+                screenHandler_Click(this, null, Settings1.Default.ScreenIndex);
+            }
+        }
+
+        private void topmostToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Settings1.Default.Topmost = this.TopMost = ((ToolStripMenuItem)sender).Checked;
+        }
+
+        private void minimiseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void screenHandler_Click(object sender, EventArgs e, int index)
+        {
+            if( index >= 0 && index < Screen.AllScreens.Count() )
+            {
+                var screen = Screen.AllScreens[index];
+                this.Location = screen.Bounds.Location;
+                this.Size = screen.Bounds.Size;
+                Settings1.Default.ScreenIndex = index;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings1.Default.Save();
+        }
+    }
+}
---
+++ BlankScreen/FormCursorAutoHide.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BlankScreen
+{
+    public class FormCursorAutoHide
+    {
+        Timer _timer;
+        System.Drawing.Point _ptHide;
+        bool _visible = true;
+        public FormCursorAutoHide(Form frm, int interval)
+        {
+            _timer = new Timer() { Interval = interval };
+            _timer.Tick += _timer_Tick;
+            frm.MouseEnter += frm_MouseEnter;
+            frm.MouseLeave += frm_MouseLeave;
+
+            frm.MouseMove += frm_MouseMove;
+        }
+
+        void frm_MouseLeave(object sender, EventArgs e)
+        {
+            _timer.Stop();
+        }
+
+        void frm_MouseMove(object sender, MouseEventArgs e)
+        {
+            Visible = true;
+            _timer.Start();
+        }
+
+        void frm_MouseEnter(object sender, EventArgs e)
+        {
+            Visible = true;
+        }
+
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            Visible = false;
+        }
+
+        public bool Visible
+        {
+            get
+            {
+                return _visible;
+            }
+            private set
+            {
+                if (value != _visible)
+                {
+                    if (value)
+                    {
+                        if (_ptHide != null && Cursor.Position.Equals(_ptHide)) // still at the location it was hidden it, don't show till it actually moves
+                        {
+                            return;
+                        }
+                        Cursor.Show();
+                    }
+                    else
+                    {
+                        _ptHide = Cursor.Position;
+                        Cursor.Hide();
+                    }
+                    _visible = value;
+                }
+            }
+        }
+    }
+}
